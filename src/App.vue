93f6e15b1b6a3d39432b61b7c49a90abb16f7d41
Garan <39609273+XtaoBai@users.noreply.github.com>
2021-03-02 11:41:45 +0800

    start with vue

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Markdown
+      v-model = "val"
+      @on-ready = "onReady"
+      @on-copy = "onCopy"
+      @on-upload-image = "onUploadImage"
+      @on-save = "onSave"
+      :height ="500"    
+    ></Markdown>
+
+  </div>
+</template>
+
+<script>
+import Markdown from './components/index'
+
+export default {
+  name: 'App',
+  components: {
+    Markdown
+  },
+  data: function(){
+    return {
+      val:''
+    }
+  },
+  methods:{
+    onReady(data){
+      console.log(data);
+    },
+    onCopy(text){
+      console.log(text);
+    },
+    onUploadImage(file){
+      console.log(file);
+    },
+    onSave(data){
+      console.log(data);
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
